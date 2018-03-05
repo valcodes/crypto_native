4e2455ec370dee9c487d31f9edcca6a1f314dc5b
Valentina Abramova <vabramova1@gmail.com>
2018-03-05 14:22:58 -0600

    initial setup

---
+++ src/Portfolio.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { ListView } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Icon,
+  List,
+  ListItem,
+  Text
+} from "native-base";
+
+export default class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Text>this is a portfolio</Text>;
+  }
+}
